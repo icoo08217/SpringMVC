42c5f588b1e15a7a98a3ac48517858b83abf052a
byungchan <icoo08216@gmail.com>
2022-02-12 20:46:48 +0900

    22.02.12

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // 스프링 MVC에서 어노테이션 기반 컨트롤러로 인식한다.
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
